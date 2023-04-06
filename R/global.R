0832d67132085878469200cfde1fc17b021b2297
sprucemister <sprucemister@gmail.com>
2023-04-05 20:32:30 -0500

    Use python virtual environments

--- R/global.R
+++ R/global.R
@@ -4,4 +4,23 @@ library(tidyverse)
 library(lubridate)
 library(shiny)
 library(shinycssloaders)
-source_python('./py/Get API Data.py')
+
+# Python Environment Setup ---------------------------------------------
+PYTHON_DEPENDENCIES = c('datetime', 'pandas', 'riotwatcher')
+
+# Create a virtual environment selecting your desired python version
+virtualenv_create(envname = "example_env_name", python= "python") #python=c(python,python3)
+# Explicitly install python libraries that you want to use, e.g. pandas, numpy
+virtualenv_install("example_env_name", packages = c('pandas','datetime','riotwatcher'))
+                   # Select the virtual environment
+                   use_virtualenv("example_env_name", required = TRUE)
+
+virtualenv_dir = Sys.getenv('VIRTUALENV_NAME')
+python_path = Sys.getenv('PYTHON_PATH')
+
+reticulate::use_virtualenv('example_env_name', required = T)
+
+print(reticulate::py_config())
+source_python('./Py/Get API Data.py')
+
+# -------------------------------------------------------------------
